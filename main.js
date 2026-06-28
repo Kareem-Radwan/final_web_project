9e20f613abfdb4ebf4fcdab7534067dc7c7b88ed
kareem-microsoft <microsoftgloballearning@gmail.com>
2026-06-28 23:26:45 +0300

    feat(backgroundChanger) adding button for changing background

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const btn = document.getElementById("btn")
 const clr = document.getElementById("clr")
+const random = document.getElementById("random")
 const txt = document.getElementById("txt")
 const navItems = document.getElementsByClassName("nav-item")
+const colors = ["red", "orange", "green"]
 
 btn.addEventListener("click", () => {
     txt.innerText = "Hello From Feature Background"
@@ -9,4 +11,9 @@ btn.addEventListener("click", () => {
 
 clr.addEventListener("click", () => {
     navItems[0].style.color = "red"
+})
+
+random.addEventListener("click", () => {
+    const rand = Math.floor(Math.random() * 3)
+    document.body.style.backgroundColor = colors[rand]
 })
