53b4c9ccda34ade106dde155c0c9395925bbe901
kareem-microsoft <microsoftgloballearning@gmail.com>
2026-06-28 23:48:14 +0300

    feat(Adding Counter Feature)

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const btn = document.getElementById("btn")
+const counter = document.getElementById("counter")
 const clr = document.getElementById("clr")
 const random = document.getElementById("random")
 const txt = document.getElementById("txt")
+const counterTxt = document.getElementById("counterTxt")
 const navItems = document.getElementsByClassName("nav-item")
 const colors = ["red", "orange", "green"]
+let start = 0
 
 btn.addEventListener("click", () => {
     txt.innerText = "Hello From Feature Background"
@@ -16,4 +19,9 @@ clr.addEventListener("click", () => {
 random.addEventListener("click", () => {
     const rand = Math.floor(Math.random() * 3)
     document.body.style.backgroundColor = colors[rand]
+})
+
+counter.addEventListener("click", function() {
+    start++
+    counterTxt.innerText = start
 })
