135de503973c15f079c3e8ba6271ccc8d1cc69a9
kareem-microsoft <microsoftgloballearning@gmail.com>
2026-06-28 23:14:42 +0300

    Adding navbar to the website with js functions

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const btn = document.getElementById("btn")
+const clr = document.getElementById("clr")
 const txt = document.getElementById("txt")
+const navItems = document.getElementsByClassName("nav-item")
 
 btn.addEventListener("click", () => {
     txt.innerText = "Hello From Js"
+})
+
+clr.addEventListener("click", () => {
+    navItems[0].style.color = "red"
 })
